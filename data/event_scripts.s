b9c26c970d135f3ba14fd7eb8eb8379ee14edd50
xMyDayyy <m.steves@web.de>
2026-08-07 22:13:45 +0000

    Hoenn-Ankunftszustand auch fuer bestehende Spielstaende

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1854,6 +1854,7 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+	.include "data/scripts/hoenn_ankunft.inc"
 	
 
 .if IS_HNS
