35a0a9d74443ae67b43b2a66cb66d7662ae76b52
xMyDayyy <m.steves@web.de>
2026-08-12 09:42:01 +0000

    Testsprung Ergänzt

--- src/debug.c
+++ src/debug.c
@@ -1853,6 +1853,8 @@ static void DebugAction_Util_HnsFinishJohtoKanto(u8 taskId)
     FlagSet(FLAG_SYS_GAME_CLEAR);
     FlagSet(FLAG_SYS_POKEDEX_GET);
     FlagSet(FLAG_VISITED_KANTO);
+    FlagSet(FLAG_IS_CHAMPION);
+    FlagSet(FLAG_IS_KANTO_CHAMPION);
     FlagSet(FLAG_RECEIVED_PASS_BINDER);
     // Flugpunkte Johto+Kanto (Hoenn bleibt bewusst unbesucht)
     for (i = 0; i < ARRAY_COUNT(sJkVisitedFlags); i++)
